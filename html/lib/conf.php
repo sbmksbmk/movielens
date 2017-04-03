40badf218c2537ed7a6d5a0b102a95017e8a331b
John Lee <john.lee@hopebaytech.com>
2017-04-03 13:37:16 +0800

    movie service path to localhost

--- html/lib/conf.php
+++ html/lib/conf.php
@@ -1,7 +1,5 @@
 <?php
 $apiurl = "http://localhost:5678/";
-$apiurl = "http://192.168.43.8:5678/";
-$apiurl = "http://10.96.19.217:5678/";
 $sessionID = 'member_id';
 $display = 'display';
 $guest_rating = 'guest_rating';
