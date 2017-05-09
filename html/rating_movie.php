5b7637a90b49a8720914a50f7e8669071d651d61
John Lee <john.lee@hopebaytech.com>
2017-05-09 16:42:10 +0800

    add some comments

--- html/rating_movie.php
+++ html/rating_movie.php
@@ -20,13 +20,16 @@ if (preg_match("/^\d+$/", $_GET['movieid']) && preg_match("/^\d+(\.\d{1,2})?/", 
         include_once("lib/db.php");
         //save rating into db
         $sn = $_SESSION[$sessionID] . str_pad($_GET['movieid'], 5, '0', STR_PAD_LEFT);
+        # after rating set member done_rate with 1 in SESSION
         $_SESSION[$done_rate] = 1;
         $time = date('Y-m-d H:i:s');
+        # insert or update rating data into db
         $str = "insert into member_rating (sn, member_id, movieid, rating, ratingtime) values " .
                "('" . $sn . "', '" . $_SESSION[$sessionID] . "', " . $_GET['movieid'] . ", " .
                $_GET['rating'] . ", '" . $time . "') " .
                "on duplicate key update rating = " . $_GET['rating'] . ", ratingtime = '" . $time . "'";
         mysql_query($str);
+        # update member done_rate with 1
         $str = "update member set done_rate = 1 where userid = '" . $_SESSION[$sessionID] . "'";
         mysql_query($str);
     }
